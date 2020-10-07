5288fe6db6e368ce67c34a916916a58f4c38e0b0
OceanNuclear <annoyingamygdala@gmail.com>
2020-10-07 18:25:38 +0100

    first commit

---
+++ dual.py
@@ -0,0 +1,20 @@
+from numpy import array as ary; from numpy import log as ln
+from numpy import cos, sin, pi, sqrt, exp, arccos;
+tau = 2*pi
+import numpy as np;
+from matplotlib import pyplot as plt
+
+class DualNumberArray(numpy.ndarray):
+    def __mul__():
+
+    def __add__():
+
+
+class DualNumber(np.complex):
+    pass
+    def __mul__():
+
+    def __add__():
+
+if __name__=='__main__':
+    pass
